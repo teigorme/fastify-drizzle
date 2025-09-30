23cbafbf8aa27322e18ce98dbb5aba685eb3004a
teigorme <igorteixeira354@gmail.com>
2025-09-30 22:45:06 +0100

    feat: add fastify-type-provider-zod

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,56 @@
 import fastify from "fastify";
+import * as z from "zod";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
-app.get("/", () => {
-  return "Hello World";
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Fastify API docs",
+      description: "Simple server with fastify",
+      version: "1.0.0",
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
+
+app.after(() => {
+  app.withTypeProvider<ZodTypeProvider>().route({
+    method: "GET",
+    url: "/",
+    schema: {
+      response: {
+        200: z.string().describe("Mensagem de boas-vindas"),
+      },
+    },
+    handler: (req, res) => {
+      res.send("Hello World");
+    },
+  });
 });
 
 app.listen({ port: 3333 }).then(() => {
